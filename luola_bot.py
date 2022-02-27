c6cbe403ddc1bf5f153fb95ae91f4d15eb4c3ac8
Juuso Terävä <juzasharp@gmail.com>
2022-02-27 01:52:45 +0100

    Support configurable list of forbidden commands

--- luola_bot.py
+++ luola_bot.py
@@ -2,6 +2,8 @@ import requests as req
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 DND_API_URL = "https://www.dnd5eapi.co/api"
+with open('forbidden-commands.txt', 'r') as file:
+    FORBIDDEN_COMMANDS=file.read().split('\n')
 
 # function to handle the /start command
 def start(update, context):
@@ -24,23 +26,24 @@ def text(update, context):
     text_received = update.message.text
     if text_received.startswith('/'):
         parsed_text=text_received.split(' ')
-        rule_category=parsed_text[0]
-        rule='-'.join(parsed_text[1:]).replace('\'', '').lower()
-        rule_response=req.get(f'{DND_API_URL}{rule_category}s/{rule}')
-        if rule_response.status_code == 200:
-            rule_content=rule_response.json()
-            rule_name=rule_content['name']
-            rule_desc='There is no description for this.'
-            if 'desc' in rule_content.keys():
-                rule_desc='\n'.join(rule_content['desc'])
-            update.message.reply_text(f'{rule_name}\n\n{rule_desc}')
-        else:
-            update.message.reply_text(f'Could not get rule from DnD API ({rule_response.status_code}). :(')
+        if parsed_text[0] not in FORBIDDEN_COMMANDS:
+            rule_category=parsed_text[0]
+            rule='-'.join(parsed_text[1:]).replace('\'', '').lower()
+            rule_response=req.get(f'{DND_API_URL}{rule_category}s/{rule}')
+            if rule_response.status_code == 200:
+                rule_content=rule_response.json()
+                rule_name=rule_content['name']
+                rule_desc='There is no description for this.'
+                if 'desc' in rule_content.keys():
+                    rule_desc='\n'.join(rule_content['desc'])
+                update.message.reply_text(f'{rule_name}\n\n{rule_desc}')
+            else:
+                update.message.reply_text(f'Could not get rule from DnD API ({rule_response.status_code}). :(')
 
 def main():
     with open('token.txt', 'r') as file:
-      BOT_TOKEN=file.read().replace('\n', '')
-    
+        BOT_TOKEN=file.read().replace('\n', '')
+
     # create the updater, that will automatically create also a dispatcher and a queue to 
     # make them dialoge
     updater = Updater(BOT_TOKEN, use_context=True)
