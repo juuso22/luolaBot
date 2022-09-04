925d76b3e6ccb6f93e10d8bffd64856c6b607754
Juuso Terävä <juzasharp@gmail.com>
2022-09-04 12:29:22 +0200

    Remove useless script

--- setup_luola_bot_in_k8s.sh
+++
@@ -1,3 +0,0 @@
-kubectl create secret generic luola-bot-config --from-file=luolabot.yaml
-sed s+{{pwd}}+$(pwd)+ luola-bot-manifest-template.yaml | tee luola-bot-manifest.yaml 
-kubectl apply -f statefulset-luolabot.yaml
