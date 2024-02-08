727dec0a16f67916a3a9fdeec99a6a6a7cb28d6c
Juuso Terävä <juzasharp@gmail.com>
2024-02-08 18:02:30 +0100

    Remove some commented nix code

--- operator/container.nix
+++ operator/container.nix
@@ -3,7 +3,7 @@ let
   pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz) {};
   luola-bot-operator-packages = import ./default.nix {};
   operatorFile = pkgs.writeTextDir "luola_bot_operator.py" (builtins.readFile ./luola_bot_operator.py);
-#  operatorFile = builtins.filterSource (path: type: (baseNameOf path) == "./luola_bot_operator.py") ./.;
+
 in
 pkgs.dockerTools.buildLayeredImage
   {
@@ -12,11 +12,6 @@ pkgs.dockerTools.buildLayeredImage
       operatorFile
       luola-bot-operator-packages
     ];
-#    copyToRoot = pkgs.buildEnv {
-#      name = "image-root";
-#      pathsToLink = ["/"];
-#      paths = [operatorFile];
-#    };
     config = {
       Cmd = [
         "python3"
